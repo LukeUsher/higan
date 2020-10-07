c12458bda09405a6f7dfd899725134c525188d5c
Luke Usher <luke.usher@outlook.com>
2020-10-07 16:15:30 +0100

    spec: implement Kempston joystick

--- higan/spec/expansion/kempston/kempston.h
+++ higan/spec/expansion/kempston/kempston.h
@@ -1,8 +1,14 @@
 struct Kempston : Expansion {
+  Node::Button up;
+  Node::Button down;
+  Node::Button left;
+  Node::Button right;
+  Node::Button fire;
+
   Kempston(Node::Port parent);
 
   auto romcs() -> bool { return false; }
-  auto mapped(uint16 address, bool io) -> bool { return io && address == 0x1f; }
+  auto mapped(uint16 address, bool io) -> bool { return io && (uint8)address == 0x1f; }
 
   auto read(uint16 address) -> uint8 { return 0xff; }
   auto write(uint16 address, uint8 data) -> void {}
